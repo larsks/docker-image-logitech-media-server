7293778ea11fb23eb806001a9ff3aed099c538c2
Richard van den Berg <richard@vdberg.org>
2017-01-25 19:46:55 +0100

    Run as user squeezeboxserver

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 if [ "$SQUEEZE_VOL" ] && [ -d "$SQUEEZE_VOL" ]; then
 	for subdir in prefs logs cache; do
 		mkdir -p $SQUEEZE_VOL/$subdir
+		chown -R squeezeboxserver:nogroup $SQUEEZE_VOL/$subdir
 	done
 fi
 
-exec /start-squeezebox.sh "$@"
+exec sudo -u squeezeboxserver -E /start-squeezebox.sh "$@"
 
